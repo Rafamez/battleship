e1f44f80db0a157e0219d33a63a6d33391407f33
Grace Hopper <daniel.rafail@dawsoncollege.qc.ca>
2017-11-21 14:43:53 -0500

    big changes

--- project/battleship/battleship/MainWindow.xaml.cs
+++ project/battleship/battleship/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -21,7 +22,18 @@ namespace battleship
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
 
+    }
+
+    public partial class gameWindow : MainWindow
+    {
+        int gridX = 10;
+        int gridY = 10;
         double size = 0;
         int difficulty = 0;
         System.Timers.Timer T = new System.Timers.Timer();
@@ -30,30 +42,30 @@ namespace battleship
 
 
 
-        public MainWindow()
+        public gameWindow()
         {
-            InitializeComponent();
             size = mainWindow.ActualWidth;
             //Call method to change value of GameTime when event is met
             T.Elapsed += new ElapsedEventHandler(OnTimedEvent);
             //set the interval to 1000
             T.Interval = 1000;
+            Turn.Content = "YOUR \r\nTURN";
         }
 
-      /*  private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if ((PlayerGrid.Margin.Right + AIGrid.Margin.Left < PlayerGrid.ActualWidth + AIGrid.ActualWidth) && (size - mainWindow.ActualWidth > 0))
-            {
-                PlayerGrid.Margin = new Thickness(PlayerGrid.Margin.Left, PlayerGrid.Margin.Top, size - mainWindow.ActualWidth, PlayerGrid.Margin.Bottom);
-                AIGrid.Margin = new Thickness(size - mainWindow.ActualWidth, AIGrid.Margin.Top, AIGrid.Margin.Right, AIGrid.Margin.Bottom);
-            }
-            else
-            {
-                PlayerGrid.Margin = new Thickness(PlayerGrid.Margin.Left, PlayerGrid.Margin.Top, PlayerGrid.ActualWidth / 2 + 400, PlayerGrid.Margin.Bottom);
-                AIGrid.Margin = new Thickness(AIGrid.ActualWidth / 2 + 400, AIGrid.Margin.Top, AIGrid.Margin.Right, AIGrid.Margin.Bottom);
-            }
-        }
-    */
+        /*  private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+          {
+              if ((PlayerGrid.Margin.Right + AIGrid.Margin.Left < PlayerGrid.ActualWidth + AIGrid.ActualWidth) && (size - mainWindow.ActualWidth > 0))
+              {
+                  PlayerGrid.Margin = new Thickness(PlayerGrid.Margin.Left, PlayerGrid.Margin.Top, size - mainWindow.ActualWidth, PlayerGrid.Margin.Bottom);
+                  AIGrid.Margin = new Thickness(size - mainWindow.ActualWidth, AIGrid.Margin.Top, AIGrid.Margin.Right, AIGrid.Margin.Bottom);
+              }
+              else
+              {
+                  PlayerGrid.Margin = new Thickness(PlayerGrid.Margin.Left, PlayerGrid.Margin.Top, PlayerGrid.ActualWidth / 2 + 400, PlayerGrid.Margin.Bottom);
+                  AIGrid.Margin = new Thickness(AIGrid.ActualWidth / 2 + 400, AIGrid.Margin.Top, AIGrid.Margin.Right, AIGrid.Margin.Bottom);
+              }
+          }
+      */
         //when the elapsedEventArgs e is met (1000 milliseconds elapsed)
         private void OnTimedEvent(object source, ElapsedEventArgs e)
         {
@@ -148,6 +160,73 @@ namespace battleship
 
         }
 
+        private void showCredits()
+        {
+            if (player1.getPoints > 0)
+            {
+                Credits.Visibility = Visibility.Visible;
+                CreditsValue.Visibility = Visibility.Visible;
+                CreditsValue.Text = Player1.getPoints.toString();
+            }
+        }
+    }
 
+
+    public class Player : MainWindow, ISerializable
+    {
+
+        private String username;
+        public String getName
+        {
+            get { return username; }
+            set {; }
+        }
+
+        private int points=0;
+        public int getPoints
+        {
+            get { return points;}
+            set {this.points=value;}
+        }
+        private List<List<int>> gotShot;
+
+        public Player(String name){
+            this.username = name;
+            if (name == this.username)
+            {
+             //   this.points+= //ADD SCORE VALUE FROM ETHANS CODE
+            }
+
+            }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void shoot(int x, int y)
+        {
+            if (AIGrid.RowDefinitions.ElementAt(x))
+            {
+
+            }
+            else
+            {
+
+                gotShot.Add(x);
+            }
+        }
+    }
+
+    public class Ship
+    {
+        private int[] shipLength = new int[] { 2, 3, 3, 4, 5 };
+        private String[] shipName = new String[] { "Patrol Boat (2)", "Submarine (3)", "Destroyer (3)", "Battleship (4)", "Aircraft Carrier (5)" };
+
+        public void setShip()
+        {
+
+        }
     }
 }
+
